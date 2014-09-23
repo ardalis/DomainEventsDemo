34f4d039bea518e23abbcf5ecea71c24b3018fc5
Steve Smith <steve@kentsmiths.com>
2014-09-22 22:33:58 -0400

    Added domaineventdispatcher code Added tests for Customer.UpdateName

--- DomainEventsDemo/Infrastructure/Data/DbContext.cs
+++ DomainEventsDemo/Infrastructure/Data/DbContext.cs
@@ -1,7 +1,40 @@
-﻿namespace DomainEventsDemo.Infrastructure.Data
+﻿using System.Collections.Generic;
+using System.Linq;
+using DomainEventsDemo.Model;
+using DomainEventsDemo.SharedKernel;
+
+namespace DomainEventsDemo.Infrastructure.Data
 {
     public class DbContext
     {
-         
+        private readonly IDomainEventDispatcher _domainEventDispatcher;
+
+        public DbContext(IDomainEventDispatcher domainEventDispatcher)
+        {
+            _domainEventDispatcher = domainEventDispatcher;
+        }
+
+        private static IDictionary<int, Customer> _customers = new Dictionary<int, Customer>();
+
+
+        public static IDictionary<int, Customer> Customers
+        {
+            get { return _customers; }
+        }
+
+        public void SaveChanges()
+        {
+            // dispatch events on any entities that have changes detected
+            var changedCustomers = _customers;
+            foreach (var customer in changedCustomers.Values)
+            {
+                foreach (var domainEvent in customer.Events)
+                {
+                    _domainEventDispatcher.Dispatch((dynamic)domainEvent);
+                }
+            }
+
+            // save changes - for demo purposes we're skipping this part
+        }
     }
 }
---
+++ DomainEventsDemo/Tests/CounterIncrementShould - Copy.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DomainEventsDemo.Model;
+using DomainEventsDemo.Model.Events;
+using DomainEventsDemo.SharedKernel;
+using NUnit.Framework;
+
+namespace DomainEventsDemo.Tests
+{
+    [TestFixture]
+    public class CustomerUpdateNameShould
+    {
+        private readonly string _testCustomerName = "ardalis";
+
+        [Test]
+        public void ChangeCustomerName()
+        {
+            var customer = new Customer("Steve");
+            customer.UpdateName(_testCustomerName);
+            Assert.AreEqual(_testCustomerName, customer.Name);
+        }
+
+        [Test]
+        public void RecordCustomerNameUpdatedEvent()
+        {
+            var customer = new Customer("Steve");
+            customer.UpdateName(_testCustomerName);
+
+            Assert.AreEqual(1, customer.Events.Count);
+            var updatedEvent = customer.Events.FirstOrDefault() as CustomerNameUpdatedEvent;
+            Assert.AreEqual("Steve", updatedEvent.OldName);
+            Assert.AreEqual(_testCustomerName, updatedEvent.NewName);
+        }
+    }
+}
