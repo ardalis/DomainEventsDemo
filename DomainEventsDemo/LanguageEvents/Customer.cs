e975e6c23b4a251ecce24dff9d2b1fa09487e14f
Steve Smith <steve@kentsmiths.com>
2015-08-24 11:22:15 -0400

    added sample using c# events and handlers

---
+++ DomainEventsDemo/LanguageEvents/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainEventsDemo.LanguageEvents
+{
+    public delegate void CustomerNameChangedEventHandler(object sender, EventArgs e);
+
+    public class Customer
+    {
+        public event CustomerNameChangedEventHandler NameChanged;
+
+        public string Name { get; private set; }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+            NameChanged(this, EventArgs.Empty);
+        }
+    }
+
+    public class SampleClient
+    {
+        private readonly Customer _customer;
+        private StringBuilder _output = new StringBuilder();
+
+        public SampleClient(Customer customer)
+        {
+            _customer = customer;
+            _customer.NameChanged += CustomerOnNameChanged;
+        }
+
+        public void UpdateCustomer(string newname)
+        {
+            _customer.UpdateName(newname);
+        }
+
+        public string Output()
+        {
+            return _output.ToString();
+        }
+
+        private void CustomerOnNameChanged(object sender, EventArgs eventArgs)
+        {
+            _output.Append("Customer name changed");
+        }
+
+        public void Detach()
+        {
+            _customer.NameChanged -= CustomerOnNameChanged;
+        }
+    }
+}
---
+++ DomainEventsDemo/Tests/LanguageEvents/CounterIncrementShould.cs
@@ -0,0 +1,28 @@
+﻿using DomainEventsDemo.LanguageEvents;
+using DomainEventsDemo.Model.Events;
+using DomainEventsDemo.SharedKernel.StaticApproach;
+using NUnit.Framework;
+using StructureMap;
+
+namespace DomainEventsDemo.Tests
+{
+    [TestFixture]
+    public class SampleClientShould
+    {
+        private SampleClient _sampleClient;
+        private Customer _customer;
+        [SetUp]
+        public void SetUp()
+        {
+            _customer = new Customer();
+            _sampleClient = new SampleClient(_customer);
+        }
+        [Test]
+        public void LogToOutputWhenNameUpdated()
+        {
+            _sampleClient.UpdateCustomer("Steve");
+            Assert.AreEqual("Customer name changed", _sampleClient.Output());
+        }
+
+    }
+}
