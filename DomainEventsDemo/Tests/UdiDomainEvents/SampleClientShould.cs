0dbdf2b7cd8bb90993869656708d4aba2e52c75c
Steve Smith <steve@kentsmiths.com>
2015-08-24 16:37:58 -0400

    Using the pre-existing static implementation

---
+++ DomainEventsDemo/Tests/UdiDomainEvents/SampleClientShould.cs
@@ -0,0 +1,24 @@
+﻿using DomainEventsDemo.UdiDomainEvents;
+using NUnit.Framework;
+
+namespace DomainEventsDemo.Tests.UdiDomainEvents
+{
+    [TestFixture]
+    public class SampleClientShould
+    {
+        private SampleClient _sampleClient;
+        private Customer _customer;
+        [SetUp]
+        public void SetUp()
+        {
+            _customer = new Customer();
+            _sampleClient = new SampleClient(_customer);
+        }
+        [Test]
+        public void LogToOutputWhenNameUpdated()
+        {
+            _sampleClient.UpdateCustomer("Steve");
+            Assert.AreEqual("Customer name changed", _sampleClient.Output());
+        }
+    }
+}
---
+++ DomainEventsDemo/UdiDomainEvents/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using DomainEventsDemo.SharedKernel;
+using DomainEventsDemo.SharedKernel.StaticApproach;
+
+namespace DomainEventsDemo.UdiDomainEvents
+{
+    public class Customer
+    {
+        public string Name { get; private set; }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+            DomainEvents.Raise(new CustomerNameUpdatedEvent(this));
+        }
+    }
+
+    public class CustomerNameUpdatedEvent : DomainEvent
+    {
+        public override DateTime DateTimeEventOccurred { get; protected set; }
+        public Customer Customer { get; }
+
+        public CustomerNameUpdatedEvent(Customer customer)
+        {
+            Customer = customer;
+            this.DateTimeEventOccurred = DateTime.Now;
+        }
+    }
+
+    public class SampleClient
+    {
+        private readonly Customer _customer;
+        private StringBuilder _output = new StringBuilder();
+
+        public SampleClient(Customer customer)
+        {
+            _customer = customer;
+            DomainEvents.Register< CustomerNameUpdatedEvent>(CustomerOnNameChanged);
+        }
+
+        public void UpdateCustomer(string newname)
+        {
+            _customer.UpdateName(newname);
+        }
+
+        public string Output()
+        {
+            return _output.ToString();
+        }
+
+        public void CustomerOnNameChanged(CustomerNameUpdatedEvent customerNameUpdatedEvent)
+        {
+            _output.Append("Customer name changed");
+        }
+    }
+}
